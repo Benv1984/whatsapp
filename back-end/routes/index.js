631dc754039dcc02e2b9b15aac28c12d46f08120
Benv1984 <47241499+Benv1984@users.noreply.github.com>
2022-08-04 19:52:06 +0300

    pp

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,7 +1,8 @@
 import { Controller } from '../app/controllers/index.js'
 import { Validaiton} from '../utility/validations.js'
-import { json } from 'express'
+import express, { json } from 'express'
 import cors from 'cors'
+import path from 'path'
 
 const applyRoutes = (app) => {
     app.use(cors())
@@ -15,5 +16,6 @@ app.post('/channel', Validaiton.validateCreateChannel,Controller.createChannel)
 app.get('/search-user', Validaiton.validateSearchUser,Controller.searchUser)
 app.get('/channel-list', Validaiton.validateGetChannelList,Controller.getChannelList)
 app.post('/message', Validaiton.validateAddMessage,Controller.sendMessage)
+app.get('/*', express.static(path.join(__dirname, 'front-end/public/index.html')))
 }
 export default applyRoutes
